6e156d850ff707638cd91dadd489289cca27e3de
Arista Soluciones <isc061990@outlook.com>
2023-12-06 16:33:07 -0600

    agregando descrgar ficha

--- resources/views/components/acciones.blade.php
+++ resources/views/components/acciones.blade.php
@@ -6,11 +6,11 @@
         <a href='{{ url("candidatura/{$attributes['id']}") }}' class="btn btn-primary " title="Editar">
             <i class="fas fa-edit" aria-hidden="true"></i>
         </a>
-        <button class="btn btn-danger btn-circle" title="Eliminar">
-            <i class="fas fa-trash" aria-hidden="true"></i>
-        </button>
         <button class="btn btn-success btn-circle" wire:click="$emitTo('usuarios.formulario', 'crearUsuarioCandidato',{{ $attributes['id'] }} )" title="Generar usuario">
             <i class="fas fa-user" aria-hidden="true"></i>
+        </button>
+        <button class="btn btn-warning btn-circle text-white" wire:click="generarFicha({{ $attributes['id'] }} )" title="Generar solicitud a pdf">
+            <i class="fas fa-file-pdf" aria-hidden="true"></i>
         </button>
     </div>
 </div>
