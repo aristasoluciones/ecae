3f61e1915f303d8ddf91c112474bfe8cc76da8a6
pfernand3z <pedrofernandez1982@gmail.com>
2024-01-03 18:17:42 -0600

    acento

--- resources/views/aspirantes/acuse.blade.php
+++ resources/views/aspirantes/acuse.blade.php
@@ -256,7 +256,7 @@ img{
                     <td class="grisp1" colspan="2" style="text-align: center;">{{ $aspirante->seccion_electoral[2] }}</td>
                     <td class="grisp1" colspan="2" style="text-align: center;">{{ $aspirante->seccion_electoral[3] }}</td>
                 <tr>
-                    <td class="blancop1" align="center" colspan="8">Seccion electoral</td>
+                    <td class="blancop1" align="center" colspan="8">Sección electoral</td>
                 </tr>
             
         </tbody>
