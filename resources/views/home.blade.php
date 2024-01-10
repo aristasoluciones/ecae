f8dd636be49c12921b29566eefcb62fd813e9bfa
Arista Soluciones <isc061990@outlook.com>
2024-01-10 12:17:57 -0600

    cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Inicio')
 
 @section('content_header')
-    <h1 class="m-0 text-dark">Dashboard</h1>
+    <h1 class="m-0 text-dark"><i class="fa fa-chart-pie"></i> Dashboard</h1>
 @stop
 
 @section('content')
