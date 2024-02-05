9ec6c3baa4058b3b2da9cef9a84b4acfa728a700
pfernand3z <pedrofernandez1982@gmail.com>
2024-02-05 14:46:24 -0600

    Cambios 01

--- resources/views/aspirantes/acuse.blade.php
+++ resources/views/aspirantes/acuse.blade.php
@@ -484,7 +484,13 @@ img{
         <tbody>
         <tbody>
             <tr>
-                <td class="rosap1" style="width: 65%; text-align: center;">{{mb_strtoupper ($aspirante->dom_calle)}}, <span>NO. EXTERIOR {{mb_strtoupper ($aspirante->dom_num_exterior)}},</span> <span>NO. INTERIOR {{mb_strtoupper ($aspirante->dom_num_interior)}}</span>&nbsp;
+                <td class="rosap1" style="width: 65%; text-align: center;">{{mb_strtoupper ($aspirante->dom_calle)}}
+                @if ($aspirante->dom_num_exterior)
+                <span>NO. EXTERIOR {{mb_strtoupper ($aspirante->dom_num_exterior)}}</span>
+                @else <span></span> @endif
+                @if ($aspirante->dom_num_interior)
+                <span>NO. INTERIOR {{mb_strtoupper ($aspirante->dom_num_interior)}}</span>
+                @else <span></span> @endif
                 </td>
                 <td class="rosap1" style="width: 35%; text-align: center;">{{mb_strtoupper ($aspirante->dom_colonia)}}&nbsp;
                 </td>
@@ -916,12 +922,12 @@ img{
                         <span style="font-weight: bold;">X</span>
                     @endif
                     </td>
-                <td class="blancop1" style="width: 50%; text-align: center;">¿Cuáles el motivo por el que quiere participar como SE o CAE Local?</td>
+                <td class="blancop1" style="width: 50%; text-align: center;">¿Cuál es el motivo por el que quiere participar como SE o CAE Local?</td>
             </tr>
         </tbody>
         </tbody>
     </table>
-    <table class="enmedio">
+    <table style="table-layout: fixed; height: 25px;" class="final" >
         <tbody>
         <tbody>
             <tr>
@@ -931,14 +937,8 @@ img{
                         <span style="font-weight: bold;">X</span>
                     @endif
                     </td>
-                <td class="rosap1" style="width: 50%; text-align: center;">{{ mb_strtoupper ($aspirante->motivo_secae)}}&nbsp;</td>
+                <td class="rosap1" rowspan="3" style="width: 55%; text-align: justify;" VALIGN="top">{{ mb_strtoupper ($aspirante->motivo_secae)}}</td>
             </tr>
-        </tbody>
-        </tbody>
-    </table>
-    <table class="enmedio">
-        <tbody>
-        <tbody>
             <tr>
                 <td class="grisp1" style="width: 15%; text-align: left;">Doctorado</td>
                 <td class="rosap1" style="width: 35%; text-align: center;">
@@ -946,23 +946,18 @@ img{
                         <span style="font-weight: bold;">X</span>
                     @endif
                     </td>
-                <td class="rosap1" style="width: 50%; text-align: center;">&nbsp;</td>
             </tr>
-        </tbody>
-        </tbody>
-    </table>
-    <table class="final">
-        <tbody>
-        <tbody>
             <tr>
-                <td class="rosap1" colspan="2"
+                <td class="rosap1" 
                     style="width: 20%; text-align: left;">¿Realiza estudios actualmente?<br>Especifique:&nbsp;</td>
                 <td class="rosap1" style="width: 30%; text-align: center;">{{ mb_strtoupper ($aspirante->realiza_estudios)}}</td>
-                <td class="rosap1" style="width: 50%; text-align: center;">&nbsp;</td>
+                
             </tr>
         </tbody>
         </tbody>
     </table>
+    
+    
     <!-- TERMINA SECCION 03 -->
     <!-- INICIA SECCION 04 -->
     <table class="inicio">
@@ -971,7 +966,7 @@ img{
             <tr>
                 <td class="blancop1" colspan="2"
                     style="width: 100; text-align: center;">
-                    EXPERIENCIA<br>(Señale los tres últimos empleoso prestaciones de servicios. El no contar con experiencia no será causa de exclusión)
+                    EXPERIENCIA<br>(Señale los tres últimos empleos o prestaciones de servicios. El no contar con experiencia no será causa de exclusión)
                     </br></td>
             </tr>
         </tbody>
@@ -1166,7 +1161,7 @@ img{
     <table class="enmedio">
         <tbody>
             <tr>
-                <td class="grisp2" style="width: 40%; text-align: left;">2. ¿Tienedisponibilidad de tiempo para prestar sus servicios en horario fuera de lo habitual?</td>
+                <td class="grisp2" style="width: 40%; text-align: left;">2. ¿Tiene disponibilidad de tiempo para prestar sus servicios en horario fuera de lo habitual?</td>
                 <td class="grisp2" style="width: 5%; text-align: center; position: relative;">
                     @if ($aspirante->p2_disponibilidad=='Si')
                     <span style="font-size: 10pt; font-weight: bold; position: absolute; text-align: center; right: 0; left: 0; top: 3; bottom: 0; margin: auto;">X</span>
@@ -1315,7 +1310,7 @@ img{
     <table class="enmedio">
         <tbody>
             <tr>
-                <td class="grisp2" style="width: 40%; text-align: left;">6. ¿Haparticipado como representante de partido
+                <td class="grisp2" style="width: 40%; text-align: left;">6. ¿Ha participado como representante de partido
                     político con registro vigente, candidatura independiente registrada en el PE 2023-2024 o coalición en alguna elección realizada en los últimos tres años?</td>
 
                 <td class="grisp2" style="width: 5%; text-align: center; position: relative;">
@@ -1381,7 +1376,7 @@ img{
                     @endif
                 </td>
 
-                <td colspan="4" class="rosap2" style="width: 50%; text-align: center;">15.1 En caso dehaber señalado “Sí” en la pregunta 15, marque con una “X” *</td>
+                <td colspan="4" class="rosap2" style="width: 50%; text-align: center;">15.1 En caso de haber señalado “Sí” en la pregunta 15, marque con una “X” *</td>
             </tr>
             <tr>
                 <td class="rosap2" style="width: 12.5%; text-align: center; position: relative;">
