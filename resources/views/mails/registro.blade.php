3d5d5f8beb6fd135d542de9a6dcc6db2f2c3077c
pfernand3z <pedrofernandez1982@gmail.com>
2024-03-07 23:39:41 -0600

    cambios correo

--- resources/views/mails/registro.blade.php
+++ resources/views/mails/registro.blade.php
@@ -1,127 +1,83 @@
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:center;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>&iexcl;Hola estimado/a
-            aspirante tu registro a&uacute;n NO concluye!</span></strong></p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:justify;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>Agradecemos tu
-            inter&eacute;s en participar para cargos de Supervisor Asistente
-            Electoral Local (SEL) y/o Capacitador Asistente Electoral Local
-            (CAEL), para el Proceso Electoral Local Ordinario
-            2024.</span></strong></p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:justify;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>El proceso de
-            selecci&oacute;n consta de:</span></strong></p>
-<ul style="list-style-type: disc;">
-    <li><strong><span style='font-family:"Arial",sans-serif;'>Pl&aacute;tica de
-                Inducci&oacute;n</span></strong></li>
-    <li><strong><span style='font-family:"Arial",sans-serif;'>Revisi&oacute;n
-                documental</span></strong></li>
-    <li><strong><span
-                style='font-family:"Arial",sans-serif;'>Examen;</span></strong>
+<p><strong>&iexcl;Hola estimado/a aspirante!</strong></p>
+<p>&nbsp;</p>
+<p>Solo falta un paso m&aacute;s para concluir tu registro. Deber&aacute;s
+    presentarte en las oficinas de tu Consejo Municipal Electoral m&aacute;s
+    cercano entre el <strong><u>01 de marzo y el 06 de abril de
+            2024</u></strong>, llevando la siguiente documentaci&oacute;n:</p>
+<p>&nbsp;</p>
+<ol>
+    <li>Solicitud correctamente llenada y firmada.</li>
+    <li>Acta de nacimiento (original o copia certificada y copia simple), o en
+        su caso, Carta de Naturalizaci&oacute;n.</li>
+    <li>Credencial para Votar Vigente o comprobante de tr&aacute;mite.</li>
+    <li>Comprobante de domicilio (Agua, luz, tel&eacute;fono, constancia de
+        domicilio expedida por autoridad competente).</li>
+    <li>Constancia de estudios (no tira de materias) acreditando, como
+        m&iacute;nimo, el nivel de educaci&oacute;n media b&aacute;sica
+        (secundaria).</li>
+    <li>Declaratoria bajo protesta de decir verdad (firmada).</li>
+</ol>
+<p>&nbsp;</p>
+<p>Adem&aacute;s, algunos documentos opcionales complementarios que pueden
+    fortalecer tu perfil son:</p>
+<p>&nbsp;</p>
+<ol>
+    <li>Carta que acredite tu experiencia como docente, manejo o trato de grupos
+        de personas.</li>
+    <li>Constancia de participaci&oacute;n en alg&uacute;n Proceso Electoral
+        Concurrente, Federal o Local.</li>
+    <li>Licencia de manejo vigente.</li>
+</ol>
+<p>&nbsp;</p>
+<p>Agradecemos tu inter&eacute;s en participar como Supervisor/a Asistente
+    Electoral Local (SEL) y/o Capacitador/a Asistente Electoral Local (CAEL) en
+    este Proceso Electoral Local Ordinario 2024.</p>
+<p>&nbsp;</p>
+<p>Es esencial que tengas presente las siguientes etapas del proceso de
+    selecci&oacute;n:</p>
+<p>&nbsp;</p>
+<ol>
+    <li><strong>Pl&aacute;tica de Inducci&oacute;n:</strong> En esta fase,
+        recibir&aacute;s informaci&oacute;n detallada sobre las
+        responsabilidades y expectativas del cargo que est&aacute;s
+        considerando.</li>
+</ol>
+<p>&nbsp;</p>
+<ol start="2">
+    <li><strong>Revisi&oacute;n Documental:</strong> Verificaremos la
+        documentaci&oacute;n presentada para asegurarnos de que cumple con los
+        requisitos establecidos en la convocatoria.</li>
+</ol>
+<p>&nbsp;</p>
+<ol start="3">
+    <li><strong>Examen:</strong> Este evaluar&aacute; tus conocimientos
+        relacionados con el proceso electoral y las funciones del puesto.</li>
+</ol>
+<p>&nbsp;</p>
+<ol start="4">
+    <li><strong>Entrevista:</strong> Brindar&aacute; la oportunidad de compartir
+        m&aacute;s acerca de tu experiencia, habilidades y motivaci&oacute;n
+        para desempe&ntilde;ar este rol.</li>
+</ol>
+<p>En caso de ser contratado/a, ser&aacute; necesario que entregues la siguiente
+    documentaci&oacute;n:</p>
+<p>&nbsp;</p>
+<ol>
+    <li>Copia de la Clave &Uacute;nica del Registro de Poblaci&oacute;n (CURP).
     </li>
-    <li><strong><span style='font-family:"Arial",sans-serif;'>y
-                Entrevista.</span></strong></li>
-</ul>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:center;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>&iexcl;Ac&eacute;rcate
-            y participa!</span></strong></p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:justify;'>
-    <span style='font-family:"Arial",sans-serif;'>Te invitamos a revisar los
-        requisitos LEGALES Y ADMINISTRATIVOS estipulado en la
-        <em>Convocatoria</em>.</span></p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:justify;'>
-    <span style='font-family:"Arial",sans-serif;'>Asimismo, deber&aacute;s de
-        acudir a las oficinas de tu Consejo Municipal Electoral m&aacute;s
-        cercano para concluir tu registro con la siguiente
-        documentaci&oacute;n:</span></p>
-<ol style="list-style-type: decimal;">
-    <li><span style='font-family:"Arial",sans-serif;'>Solicitud correctamente
-            llenada y firmada.</span></li>
-    <li><span style='font-family:"Arial",sans-serif;'>Acta de nacimiento
-            (original o copia certificada y copia simple), o en su caso, Carta
-            de Naturalizaci&oacute;n.</span></li>
-    <li><span style='font-family:"Arial",sans-serif;'>Credencial para Votar
-            Vigente o comprobante de tr&aacute;mite.</span></li>
-    <li><span style='font-family:"Arial",sans-serif;'>Comprobante de domicilio
-            (Agua, luz, tel&eacute;fono, constancia de domicilio expedida por
-            autoridad competente).</span></li>
-    <li><span style='font-family:"Arial",sans-serif;'>Acreditado, como
-            m&iacute;nimo, el nivel de educaci&oacute;n media b&aacute;sica
-            (secundaria), por lo que deber&aacute; presentar la Constancia de
-            estudios (<strong>no tira de materias</strong>).</span></li>
-    <li><span style='font-family:"Arial",sans-serif;'>Declaratoria bajo protesta
-            de decir verdad (firmada)</span></li>
+    <li>Copia del Registro Federal de Contribuyentes (RFC).</li>
+    <li>Tres fotograf&iacute;as tama&ntilde;o infantil a color o en blanco y
+        negro.</li>
 </ol>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:center;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>&nbsp;</span></strong>
-</p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:center;line-height:normal;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>Documentos opcionales
-            complementarios&nbsp;</span></strong></p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:center;line-height:normal;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>(El no contar con
-            ellos no ser&aacute; causa de exclusi&oacute;n de la persona
-            aspirante)</span></strong></p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:center;line-height:normal;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>&nbsp;</span></strong>
-</p>
-<ol style="list-style-type: decimal;">
-    <li><span style='font-family:"Arial",sans-serif;'>Carta que acredite su
-            experiencia como docente, manejo o trato de grupos de
-            personas.</span></li>
-    <li><span style='font-family:"Arial",sans-serif;'>Constancia de
-            participaci&oacute;n en alg&uacute;n Proceso Electoral Concurrente,
-            Federal o Local.</span></li>
-    <li><span style='font-family:"Arial",sans-serif;'>Licencia de manejo
-            vigente&nbsp;</span></li>
-</ol>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:center;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>&nbsp;</span></strong>
-</p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:center;line-height:normal;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>Asimismo, en caso de
-            ser contratado/a,&nbsp;</span></strong></p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:center;line-height:normal;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>deber&aacute; de
-            entregar la siguiente documentaci&oacute;n:</span></strong></p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:center;line-height:normal;'>
-    <strong><span style='font-family:"Arial",sans-serif;'>&nbsp;</span></strong>
-</p>
-<ol style="list-style-type: decimal;">
-    <li><span style='font-family:"Arial",sans-serif;'>Copia de la Clave
-            &Uacute;nica del Registro de Poblaci&oacute;n (CURP)</span></li>
-    <li><span style='font-family:"Arial",sans-serif;'>Copia del Registro Federal
-            de Contribuyentes (RFC)</span></li>
-    <li><span style='font-family:"Arial",sans-serif;'>Tres fotograf&iacute;as
-            tama&ntilde;o infantil a color o en blanco y negro&nbsp; &nbsp;
-            &nbsp; &nbsp; &nbsp; &nbsp;</span></li>
-</ol>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:justify;'>
-    <span style='font-family:"Arial",sans-serif;'>&nbsp;</span></p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:justify;'>
-    <span style='font-family:"Arial",sans-serif;'>Para mayores informes favor de
-        comunicarse a las Oficinas Centrales del Instituto de Elecciones y
-        Participaci&oacute;n Ciudadana de Chiapas, conmutador (961) 26 400 20,
-        26 400 21, 26 400 22, 26 400 23, Lada sin costo 01800 050 IEPC (4372),
-        extensiones: 1605,1607,1609, 1604 o al correo
-        electr&oacute;nico:&nbsp;</span></p>
-<p
-    style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;text-align:justify;'>
-    <a href="mailto:registros-selycael2024@iepc-chiapas.org.mx"><span
-            style='font-family:"Arial",sans-serif;'>registros-selycael2024@iepc-chiapas.org.mx</span></a>
-</p>
+<p>&nbsp;</p>
+<p>Si tienes alguna pregunta, no dudes en comunicarte con las Oficinas Centrales
+    del Instituto de Elecciones y Participaci&oacute;n Ciudadana de Chiapas al
+    conmutador (961) 26 400 20, 26 400 21, 26 400 22, 26 400 23, o la Lada sin
+    costo 01800 050 IEPC (4372), extensiones: 1605, 1607, 1609, 1604, o al
+    correo electr&oacute;nico: registros-selycael2024@iepc-chiapas.org.mx.</p>
+<p>&nbsp;</p>
+<p>&iexcl;Solo falta un paso m&aacute;s para concluir tu registro! Te animamos a
+    completarlo y te deseamos &eacute;xito en este proceso.</p>
+<p>&nbsp;</p>
+<p>&iexcl;Esperamos contar con tu valiosa participaci&oacute;n durante el
+    Proceso Electoral Local Ordinario 2024!</p>
