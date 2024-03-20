390d1ac33f7c11f9317a39c206d6b8d096b7740c
pfernand3z <pedrofernandez1982@gmail.com>
2024-03-20 10:27:47 -0600

    actualización correo

--- resources/views/mails/registro.blade.php
+++ resources/views/mails/registro.blade.php
@@ -1,83 +1,44 @@
 <p><strong>&iexcl;Hola estimado/a aspirante!</strong></p>
-<p>&nbsp;</p>
-<p>Solo falta un paso m&aacute;s para concluir tu registro. Deber&aacute;s
-    presentarte en las oficinas de tu Consejo Municipal Electoral m&aacute;s
-    cercano entre el <strong><u>01 de marzo y el 06 de abril de
-            2024</u></strong>, llevando la siguiente documentaci&oacute;n:</p>
-<p>&nbsp;</p>
+<p>Solo falta un paso m&aacute;s para concluir tu registro. Deber&aacute;s presentarte en las oficinas de tu Consejo Municipal Electoral m&aacute;s cercano entre el <strong><u>01 de marzo y el 06 de abril de 2024</u></strong>, llevando la siguiente documentaci&oacute;n:</p>
+<p>Ubica la direcci&oacute;n de tu Consejo Municipal m&aacute;s cercano aqu&iacute;:</p>
+<p><a href="https://www.iepc-chiapas.org.mx/organos-desconcentrados-2024">https://www.iepc-chiapas.org.mx/organos-desconcentrados-2024</a></p>
+<p>Descarga tu gu&iacute;a de estudio aqu&iacute;:</p>
+<p><a href="https://www.iepc-chiapas.org.mx/sel-y-cael-pelo2024">https://www.iepc-chiapas.org.mx/sel-y-cael-pelo2024</a></p>
+<p>Documentaci&oacute;n que deber&aacute;s presentar:</p>
 <ol>
-    <li>Solicitud correctamente llenada y firmada.</li>
-    <li>Acta de nacimiento (original o copia certificada y copia simple), o en
-        su caso, Carta de Naturalizaci&oacute;n.</li>
-    <li>Credencial para Votar Vigente o comprobante de tr&aacute;mite.</li>
-    <li>Comprobante de domicilio (Agua, luz, tel&eacute;fono, constancia de
-        domicilio expedida por autoridad competente).</li>
-    <li>Constancia de estudios (no tira de materias) acreditando, como
-        m&iacute;nimo, el nivel de educaci&oacute;n media b&aacute;sica
-        (secundaria).</li>
-    <li>Declaratoria bajo protesta de decir verdad (firmada).</li>
+<li>Solicitud correctamente llenada y firmada.</li>
+<li>Acta de nacimiento (original o copia certificada y copia simple), o en su caso, Carta de Naturalizaci&oacute;n.</li>
+<li>Credencial para Votar Vigente o comprobante de tr&aacute;mite.</li>
+<li>Comprobante de domicilio (Agua, luz, tel&eacute;fono, constancia de domicilio expedida por autoridad competente).</li>
+<li>Constancia de estudios (no tira de materias) acreditando, como m&iacute;nimo, el nivel de educaci&oacute;n media b&aacute;sica (secundaria).</li>
+<li>Declaratoria bajo protesta de decir verdad (firmada).</li>
+<li>Copia de la Clave &Uacute;nica del Registro de Poblaci&oacute;n (CURP).</li>
+<li>Copia del Registro Federal de Contribuyentes (RFC).</li>
 </ol>
-<p>&nbsp;</p>
-<p>Adem&aacute;s, algunos documentos opcionales complementarios que pueden
-    fortalecer tu perfil son:</p>
-<p>&nbsp;</p>
+<p>En caso de ser contratado/a, ser&aacute; necesario que entregues la siguiente documentaci&oacute;n:</p>
 <ol>
-    <li>Carta que acredite tu experiencia como docente, manejo o trato de grupos
-        de personas.</li>
-    <li>Constancia de participaci&oacute;n en alg&uacute;n Proceso Electoral
-        Concurrente, Federal o Local.</li>
-    <li>Licencia de manejo vigente.</li>
+<li>Tres fotograf&iacute;as tama&ntilde;o infantil a color o en blanco y negro.</li>
 </ol>
-<p>&nbsp;</p>
-<p>Agradecemos tu inter&eacute;s en participar como Supervisor/a Asistente
-    Electoral Local (SEL) y/o Capacitador/a Asistente Electoral Local (CAEL) en
-    este Proceso Electoral Local Ordinario 2024.</p>
-<p>&nbsp;</p>
-<p>Es esencial que tengas presente las siguientes etapas del proceso de
-    selecci&oacute;n:</p>
-<p>&nbsp;</p>
+<p>Adem&aacute;s, algunos documentos opcionales complementarios que pueden fortalecer tu perfil son:</p>
 <ol>
-    <li><strong>Pl&aacute;tica de Inducci&oacute;n:</strong> En esta fase,
-        recibir&aacute;s informaci&oacute;n detallada sobre las
-        responsabilidades y expectativas del cargo que est&aacute;s
-        considerando.</li>
+<li>Carta que acredite tu experiencia como docente, manejo o trato de grupos de personas.</li>
+<li>Constancia de participaci&oacute;n en alg&uacute;n Proceso Electoral Concurrente, Federal o Local.</li>
+<li>Licencia de manejo vigente.</li>
 </ol>
-<p>&nbsp;</p>
+<p>Agradecemos tu inter&eacute;s en participar como Supervisor/a Asistente Electoral Local (SEL) y/o Capacitador/a Asistente Electoral Local (CAEL) en este Proceso Electoral Local Ordinario 2024.</p>
+<p>Es esencial que tengas presente las siguientes etapas del proceso de selecci&oacute;n:</p>
+<ol>
+<li><strong>Pl&aacute;tica de Inducci&oacute;n:</strong> En esta fase, recibir&aacute;s informaci&oacute;n detallada sobre las responsabilidades y expectativas del cargo que est&aacute;s considerando.</li>
+</ol>
 <ol start="2">
-    <li><strong>Revisi&oacute;n Documental:</strong> Verificaremos la
-        documentaci&oacute;n presentada para asegurarnos de que cumple con los
-        requisitos establecidos en la convocatoria.</li>
+<li><strong>Revisi&oacute;n Documental:</strong> Verificaremos la documentaci&oacute;n presentada para asegurarnos de que cumple con los requisitos establecidos en la convocatoria.</li>
 </ol>
-<p>&nbsp;</p>
 <ol start="3">
-    <li><strong>Examen:</strong> Este evaluar&aacute; tus conocimientos
-        relacionados con el proceso electoral y las funciones del puesto.</li>
+<li><strong>Examen:</strong> Este evaluar&aacute; tus conocimientos relacionados con el proceso electoral y las funciones del puesto.</li>
 </ol>
-<p>&nbsp;</p>
 <ol start="4">
-    <li><strong>Entrevista:</strong> Brindar&aacute; la oportunidad de compartir
-        m&aacute;s acerca de tu experiencia, habilidades y motivaci&oacute;n
-        para desempe&ntilde;ar este rol.</li>
+<li><strong>Entrevista:</strong> Brindar&aacute; la oportunidad de compartir m&aacute;s acerca de tu experiencia, habilidades y motivaci&oacute;n para desempe&ntilde;ar este rol.</li>
 </ol>
-<p>En caso de ser contratado/a, ser&aacute; necesario que entregues la siguiente
-    documentaci&oacute;n:</p>
-<p>&nbsp;</p>
-<ol>
-    <li>Copia de la Clave &Uacute;nica del Registro de Poblaci&oacute;n (CURP).
-    </li>
-    <li>Copia del Registro Federal de Contribuyentes (RFC).</li>
-    <li>Tres fotograf&iacute;as tama&ntilde;o infantil a color o en blanco y
-        negro.</li>
-</ol>
-<p>&nbsp;</p>
-<p>Si tienes alguna pregunta, no dudes en comunicarte con las Oficinas Centrales
-    del Instituto de Elecciones y Participaci&oacute;n Ciudadana de Chiapas al
-    conmutador (961) 26 400 20, 26 400 21, 26 400 22, 26 400 23, o la Lada sin
-    costo 01800 050 IEPC (4372), extensiones: 1605, 1607, 1609, 1604, o al
-    correo electr&oacute;nico: registros-selycael2024@iepc-chiapas.org.mx.</p>
-<p>&nbsp;</p>
-<p>&iexcl;Solo falta un paso m&aacute;s para concluir tu registro! Te animamos a
-    completarlo y te deseamos &eacute;xito en este proceso.</p>
-<p>&nbsp;</p>
-<p>&iexcl;Esperamos contar con tu valiosa participaci&oacute;n durante el
-    Proceso Electoral Local Ordinario 2024!</p>
+<p>Si tienes alguna pregunta, no dudes en comunicarte con las Oficinas Centrales del Instituto de Elecciones y Participaci&oacute;n Ciudadana de Chiapas al conmutador (961) 26 400 20, 26 400 21, 26 400 22, 26 400 23, o la Lada sin costo 01800 050 IEPC (4372), extensiones: 1605, 1607, 1609, 1604, o al correo electr&oacute;nico: registros-selycael2024@iepc-chiapas.org.mx.</p>
+<p>&iexcl;Solo falta un paso m&aacute;s para concluir tu registro! Te animamos a completarlo y te deseamos &eacute;xito en este proceso.</p>
+<p>&iexcl;Esperamos contar con tu valiosa participaci&oacute;n durante el Proceso Electoral Local Ordinario 2024!</p>
