2999f5ad255eda77f74084d8fc4571b8c4718b37
Arista Soluciones <isc061990@outlook.com>
2024-04-04 13:06:23 -0600

    accion de eliminar aspirante, con permisos

--- app/Http/Livewire/Aspirantes/Lista.php
+++ app/Http/Livewire/Aspirantes/Lista.php
@@ -23,6 +23,8 @@ class Lista extends DataTableComponent
 {
     protected $model = Aspirante::class;
 
+    public $registroEliminar;
+
     public $municipios;
     public $edades;
     public $generos;
@@ -41,7 +43,7 @@ class Lista extends DataTableComponent
         $this->setSearchVisibilityStatus(false);
         $this->setColumnSelectDisabled();
         $this->setPerPageAccepted([10, 25, 50, 100,500,1000,2000,3000]);
-        $this->setAdditionalSelects(['id','estatus','apellido1','apellido2','sede','email','documentacion']);
+        $this->setAdditionalSelects(['id','estatus','apellido1','apellido2','sede','email','documentacion','created_at']);
         $this->setConfigurableAreas([
             'before-tools' => [
                 'aspirantes.busqueda',
@@ -350,5 +352,25 @@ class Lista extends DataTableComponent
         $this->emitTo('aspirantes.entrevista', 'resetear');
         $this->emitTo('aspirantes.entrevista', 'setAspirante',$id);
         $this->emit('modal:show', '#modal-entrevista');
+    }
+
+    public function handlerEliminar($id) {
+
+        $this->registroEliminar = Aspirante::find($id);
+        $this->emit('modal:show', '#modal-confirmar-eliminar');
+    }
+
+    public function eliminar() {
+
+        $this->registroEliminar->delete();
+
+        $this->emit('swal:alert', [
+            'icon'    => 'success',
+            'title'   => 'Registro eliminado.',
+            'timeout' => 5000
+        ]);
+        $this->reset(['registroEliminar']);
+        $this->emit('modal:hide', '#modal-confirmar-eliminar');
+        $this->render();
     }
 }
--- resources/views/aspirantes/modal.blade.php
+++ resources/views/aspirantes/modal.blade.php
@@ -8,3 +8,46 @@
 <div id="modal-entrevista" wire:ignore.self class="modal fade" role="dialog" data-backdrop="static" data-keyboard="true">
     <livewire:aspirantes.entrevista />
 </div>
+<div id="modal-confirmar-eliminar" wire:ignore.self class="modal fade" role="dialog" data-backdrop="static"
+     data-keyboard="false">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-danger">
+                <h4 class="modal-title">Confirmar eliminar</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-12 text-justify">
+                        <p>Esta seguro de eliminar la información, tenga en cuenta que esta acción es irreversible.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <p><strong>Clave de elector : </strong><span class="badge badge-info fs-15">{{ $registroEliminar?->clave_elector }}</span></p>
+                        <p><strong>Nombre del aspirante : </strong><span class="badge badge-info fs-15">{{ $registroEliminar?->nombre." ".$registroEliminar?->apellido1." ".$registroEliminar?->apellido2 }}</span></p>
+                        <p><strong>Consejo municipal    : </strong><span class="badge badge-info fs-15">{{ $registroEliminar?->sede }}</span></p>
+                    </div>
+
+                </div>
+            </div>
+            <div class="modal-footer">
+                <div class="btn-group-toggle" wire:loading.remove wire.target="eliminar">
+                    <button type="button"
+                            class="btn btn-danger close-btn mr-2"
+                            data-dismiss="modal">Cerrar</button>
+                    <button type="button"
+                            class="btn btn-primary"
+                            wire:click="eliminar">Si</button>
+                </div>
+                <button type="button"
+                        class="btn btn-primary"
+                        wire:loading wire.target="eliminar">
+                    <span >Eliminando información <i class="fas fa-spinner"></i></span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
